b600a2d5c0bb27b79c898606c7de7fd69f53adb0
jofret <jofret_@hotmail.com>
2020-05-26 11:45:58 -0300

    ultimos cambios mañana 26-5-2020

--- resources/views/libros/inicio.blade.php
+++ resources/views/libros/inicio.blade.php
@@ -5,13 +5,14 @@
 
     <section class="text-center">
       <div class="container">
-        <h1>Nuestra Colleción</h1>
+        <h1 style="padding-bottom: 20px">Nuestra Colleción</h1>
         <!-- <p>
           <a href="#" class="btn btn-primary my-2">Main call to action</a>
           <a href="#" class="btn btn-secondary my-2">Secondary action</a>
         </p> -->
       </div>
     </section>
+
 
     <div class="album py-5 bg-light">
       <div class="container">
